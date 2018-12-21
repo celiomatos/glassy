be8a9cebd34066437152e34d8122634f79cfb13b
Celio Matos <celio.matos@itbam.org.br>
2018-12-21 15:04:17 -0400

    apos uso do projeto testando

--- src/app/core/common/material.module.ts
+++ src/app/core/common/material.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatSidenavModule,
-  MatIconModule,
-  MatFormFieldModule,
-  MatSelectModule,
-  MatTableModule,
-  MatPaginatorModule
-} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   imports: [CommonModule],
   exports: [
-    MatButtonModule,
-    MatCardModule,
+    FlexLayoutModule,
     MatSidenavModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule {}
--- src/app/core/layout/home/home.component.ts
+++ src/app/core/layout/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'gla-home',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
 
   constructor() {}
 
   ngOnInit() {}
+
+  close() {
+    this.sidenav.close();
+  }
 }
